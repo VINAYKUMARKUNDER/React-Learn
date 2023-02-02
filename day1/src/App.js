64faab2c15466f37e0879b99c78dcb21f6dfa411
VINAYKUMARKUNDER <98196841+VINAYKUMARKUNDER@users.noreply.github.com>
2023-02-02 10:44:14 +0530

    Day1

--- day1/src/App.js
+++ day1/src/App.js
@@ -1,11 +1,14 @@
 
-import './App.css';
+import styles from './App.module.css';
+import Gritting from './Componantes/Griting.jsx'
 
 function App() {
   return (
    
   <>
-  <h1>I am Vinay Kumar</h1>
+  <h1 className='styles.App'>I am Vinay Kumar</h1>
+   <Gritting/> 
+
   </>
   );
 }
---
+++ day1/src/Componantes/Griting.jsx
@@ -0,0 +1,9 @@
+import styles from "./gritting.moudle.css"
+
+export default function Gritting(){
+    return (
+        <>
+        <h1 className={styles.yellow}>Welcome in Recat first</h1>
+        </>
+    )
+}
