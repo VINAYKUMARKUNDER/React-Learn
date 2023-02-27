3c8155ee38ffbf3008e9c2b33c0833feaec6d57e
VINAYKUMARKUNDER <98196841+VINAYKUMARKUNDER@users.noreply.github.com>
2023-02-27 18:35:46 +0530

    change something

--- day3/src/App.js
+++ day3/src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import './App.css';
+import React from "react"
+
+export default function App() {
+
+
+  const [number,setNumber]=React.useState(0);
+  
+  const incriment=3
+  const dicriment=0
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  
+
+    
+    <div className='App'>
+    <h1>Count: {number}</h1>
+
+    <button onClick={incriment}>Inc</button>
+    <button onClick={dicriment}>Dic</button>
+
     </div>
+   
   );
 }
 
-export default App;
+
