36c1c45fc1e95df99a889d4dc7e7519c25792582
VINAYKUMARKUNDER <98196841+VINAYKUMARKUNDER@users.noreply.github.com>
2023-03-02 10:25:28 +0530

    day6

--- day3/src/App.js
+++ day3/src/App.js
@@ -3,41 +3,49 @@ import './App.css';
 import React from "react"
 import Item from './Componantes/Item';
 
-export default function App(){
-    const itemData=[
-        {
-            itemId:1,
-            itemName:"Noddels",
-            itemPrice:30,
-            itemCount:1
-        },
-        {
-            itemId:2,
-            itemName:"Biryani",
-            itemPrice:90,
-            itemCount:1
-        },
-        {
-            itemId:3,
-            itemName:"Chips",
-            itemPrice:10,
-            itemCount:1
-        }
+let itemData=[
+    {
+        itemId:1,
+        itemName:"Noddels",
+        itemPrice:30,
+        itemCount:1
+    },
+    {
+        itemId:2,
+        itemName:"Biryani",
+        itemPrice:90,
+        itemCount:1
+    },
+    {
+        itemId:3,
+        itemName:"Chips",
+        itemPrice:10,
+        itemCount:1
+    }
 
-    ]
+]
+
+export default function App(){
+   
 
     const style={
         border:"2px solid orange",
-        padding:"30px",
-        margin:"30px"
+        padding:"3%",
+        margin:"30px auto 30px auto",
+        width:"70%"
     }
     return (
         <div className="container" style={style}>     
         {itemData.map(name => (  
           <div key={name.itemId}>  
-            <Item itemName={name.itemName} itemPrice={name.itemPrice}/>  
+            <Item itemName={name.itemName} itemPrice={name.itemPrice} itemCount={name.itemCount}/> 
+            
           </div>  
+
+          
         ))}  
+
+        
     </div>  
     );
 }
--- day3/src/Componantes/Item.jsx
+++ day3/src/Componantes/Item.jsx
@@ -1,17 +1,36 @@
 import React from "react";
 import item from "./item.css"
 
+let total=0;
 
 export default function Item(props){
     // const [itemName, itemPrice]=[0,0];
+    // console.log(App)
+    total+=props.itemPrice;
+    
 
-   
+    const [countItem, setCountItem]=React.useState(props.itemCount);
 
+    const descHandler=()=>{
+        setCountItem(countItem-1);
+    }
+
+    const incHandler=()=>{
+        setCountItem(countItem+1);
+    }
     return (<>
         <div className="item">
-            <span>{props.itemName} </span>
-            <span>Ru. {props.itemPrice}</span>
+            
+            <div>{props.itemName} </div>
+            <div>Ru. {props.itemPrice*countItem}</div>
+            <div>
+                <button  disabled={countItem<=0} onClick={descHandler}>-</button>
+                <div>{countItem}</div>
+                <button onClick={incHandler}>+</button>
+            </div>
+            <div>Total: {total}</div>
         </div>
 
     </>)
 }
+
