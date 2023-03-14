a9a2361ee5960358fae38ad0c0cd3e33718ff198
VINAYKUMARKUNDER <98196841+VINAYKUMARKUNDER@users.noreply.github.com>
2023-03-14 06:57:32 +0530

    day3

--- day6a/src/App.js
+++ day6a/src/App.js
@@ -1,23 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+
 
 function App() {
+  const [clickBtn, setClickBtn]=React.useState(0);
+  const clickHandeler=()=>{
+    setClickBtn(clickBtn+1);
+  }
+  const resetHandeler=()=>{
+    setClickBtn(0);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Clicked ${clickBtn} times</h1>
+      <button onClick={clickHandeler}>Click me</button>
+      <button onClick={resetHandeler}>Reset</button>
     </div>
   );
 }
